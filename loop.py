9c7406851ab58444f9bc515345a7c822636b0cb2
BenestaBiseko <53521369+BenestaBiseko@users.noreply.github.com>
2019-09-18 00:02:36 -0700

    Add files via upload
    This is modified

--- loop.py
+++ loop.py
@@ -1,3 +1,26 @@
 '''
 Write a simple script that demonstrate your understanding of loops in python   
 '''
+# Start with wile loop
+'''
+d= int(input("Enter the value Multiple of 7 "))
+
+    while d %7 !=0:
+    pass d= int(input("Enter the value Multiple of 7 "))
+
+    else:
+        print("%d is Multiple of 7" %val)
+        '''
+
+
+# For Loop
+x = (1, 2, "Python")
+for i in x:
+    print(i)
+    
+# Nested Loop
+x = {[1,2,3], ["a", "b", "c"]}
+for i in x:
+    for i in j:
+        print(j, end="")
+        print()
