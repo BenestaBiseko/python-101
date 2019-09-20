7e9932c1bdf821c64035fade8ce248fc02b0c512
BenestaBiseko <53521369+BenestaBiseko@users.noreply.github.com>
2019-09-20 06:02:00 -0700

    Add files via upload

--- sets.py
+++ sets.py
@@ -1,3 +1,25 @@
 '''
 Write a simple script to demonstrate your understanding of sets as data types 
 '''
+'''
+https://github.com/BenestaBiseko/python-101/compare/master...develop
+
+'''
+
+foodset = {"beans", "cocunut", "mangoes", "meat", "peanut"}
+#del foodset
+for x in foodset:
+     print(x.upper(), end=" ") 
+
+cityset = {"Dodoma", "Arusha", "Nairobi", "Kampala"}
+cityset.update(["Beijing", "Moscow", "Lusaka"])
+cityset.remove("Beijing")
+for y in cityset:
+
+    print(y.upper(), end=" ")
+    print(len(y))
+allset = foodset.union(cityset)
+print(allset)
+cityset.update(foodset)
+print(cityset)
+
